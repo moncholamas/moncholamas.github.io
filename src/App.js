c9ae4143efab4d05baba8a15a95693db849a5098
Manuel Lamas <moncholamas@gmail.com>
2022-12-28 23:38:21 -0300

    feature: instalacion de mui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import './App.css';
+import { Container, Divider, Grid, Typography } from '@mui/material'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Prueba gh pages</h1>
-      </header>
+      <Container sx={{ backgroundColor: "#DDD" }}>
+        <Typography variant="h2" > Manuel Lamas
+          <Divider></Divider>
+          <Typography component="span">Full Stack Developer</Typography>
+        </Typography>
+        <Grid container spacing={2}>
+          <Grid item xs={8}>
+            <Typography variant="h3">Front End</Typography>
+          </Grid>
+          <Grid item xs={4}>
+            <Typography variant="h3">Back End</Typography>
+          </Grid>
+          <Grid item xs={4}>
+            <Typography variant="h3">DevOps</Typography>
+          </Grid>
+          <Grid item xs={8}>
+            <Typography variant="h3">Otros</Typography>
+          </Grid>
+        </Grid>
+      </Container>
     </div>
   );
 }
---
+++ src/containers/home/index.js
@@ -0,0 +1,7 @@
+const HomeContainer = () => {
+    return (<div>
+        Contenedor para el home
+    </div>)
+}
+
+export default HomeContainer;
